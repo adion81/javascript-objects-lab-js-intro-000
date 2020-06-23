f50d43cda59c182697df8366a267fb1dd1568593
Adrien Joseph Dion <adion81@gmail.com>
2020-06-23 03:44:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object);
+  delete object[key];
+  return object;
+}
